26d58c54f5a2950ae1a72570a74d3d5d1795412f
gogin-png <den.sukhanov.99@mail.ru>
2024-08-21 17:22:35 +0300

    works

--- CreatingModel.py
+++ CreatingModel.py
@@ -7,11 +7,12 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 # 1. Загрузка данных
-data = pd.read_csv('D:\PyCharm\TestProject-InfoTeKC\Data\dga_domains_full.csv', header=None, names=['label', 'class', 'domain'])
+data = pd.read_csv('/home/denis/PycharmProjects/TestProject-InfoTeKC-/DGA_domains_dataset-master/dga_domains_full.csv', header=None, names=['class', 'label', 'domain'])
 
 # 2. Предобработка данных
 # Преобразование меток в бинарные (0 - легитимный, 1 - DGA)
-data['label'] = LabelEncoder().fit_transform(data['label'])
+data['class'] = LabelEncoder().fit_transform(data['class'])
+
 
 # Используем Tokenizer для преобразования доменных имен в числовые последовательности
 tokenizer = Tokenizer(char_level=True)  # char_level=True означает, что рассматриваем отдельные символы
@@ -20,8 +21,9 @@ sequences = tokenizer.texts_to_sequences(data['domain'])
 
 # Дополнение или усечение последовательностей до одинаковой длины
 max_length = max([len(seq) for seq in sequences])
+print(max_length)
 X = pad_sequences(sequences, maxlen=max_length, padding='post')
-y = data['label'].values
+y = data['class'].values
 
 # 3. Разделение данных на тренировочные и тестовые
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -37,8 +39,8 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 # 6. Обучение модели
-model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
-model.save('my_model.keras')
+model.fit(X_train, y_train, epochs=8, batch_size=64, validation_data=(X_test, y_test))
+model.save('my_model-2.keras')
 
 # 7. Оценка модели
 loss, accuracy = model.evaluate(X_test, y_test)
